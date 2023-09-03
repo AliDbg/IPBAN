0c89dca3d75791cf014d06b431015a3b41939826
Ali.Debugger <ali.debugger@gmail.com>
2023-09-03 13:52:30 -0700

    Update ipban-uninstall.sh

--- ipban-uninstall.sh
+++ ipban-uninstall.sh
@@ -6,4 +6,5 @@ systemctl stop netfilter-persistent.service
 systemctl disable netfilter-persistent.service
 iptables -F && iptables -X && iptables -Z && ip6tables -F && ip6tables -X && ip6tables -Z 
 iptables-save > /etc/iptables/rules.v4 && ip6tables-save > /etc/iptables/rules.v6
+systemctl restart iptables ip6tables
 clear && echo "IPBAN Uninstalled!" 
