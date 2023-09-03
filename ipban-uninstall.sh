cb7d73fb36250971a4c8ae83221b952e9a43f261
Ali.Debugger <ali.debugger@gmail.com>
2023-09-03 13:50:37 -0700

    Add files via upload

---
+++ ipban-uninstall.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+rm "${HOME}/ipban-update.sh"
+crontab -l | grep -v "ipban-update.sh" | crontab -
+systemctl stop netfilter-persistent.service
+systemctl disable netfilter-persistent.service
+iptables -F && iptables -X && iptables -Z && ip6tables -F && ip6tables -X && ip6tables -Z 
+iptables-save > /etc/iptables/rules.v4 && ip6tables-save > /etc/iptables/rules.v6
+clear && echo "IPBAN Uninstalled!" 
