28c841c0ebeb14ed666fb96516c26ed6ed1cc927
Ali.Debugger <ali.debugger@gmail.com>
2023-09-07 13:49:59 -0700

    Add files via upload

--- ipban.sh
+++ ipban.sh
@@ -69,7 +69,7 @@ cd "\${workdir}"
 cd && rm -rf "\${workdir}"
 
 wget https://download.db-ip.com/free/dbip-country-lite-${YR}-${MON}.csv.gz -O /usr/share/xt_geoip/dbip-country-lite.csv.gz
-unzip /usr/share/xt_geoip/dbip-country-lite.csv.gz
+gunzip /usr/share/xt_geoip/dbip-country-lite.csv.gz
 /usr/lib/xtables-addons/xt_geoip_build -D /usr/share/xt_geoip/ -S /usr/share/xt_geoip/
 rm /usr/share/xt_geoip/dbip-country-lite.csv
 
@@ -77,7 +77,7 @@ modprobe x_tables && modprobe xt_geoip
 lsmod | grep ^xt_geoip
 lsmod | grep ^x_tables
 systemctl restart iptables.service ip6tables.service
-echo "Updated IPBAN!" 
+clear && echo "Updated IPBAN!" 
 EOF
 chmod +x "/usr/share/ipban/ipban-update.sh"
 }
