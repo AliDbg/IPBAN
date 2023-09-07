288dbbd3329cdf9208533dcc12e8a530b96ad738
Ali.Debugger <ali.debugger@gmail.com>
2023-09-07 15:08:23 -0700

    Add files via upload

--- ipban.sh
+++ ipban.sh
@@ -108,8 +108,9 @@ iptables_rules(){
 
 install_ipban(){
 	apt -y update && apt -y upgrade
-	apt -y install curl unzip gzip tar perl xtables-addons-common xtables-addons-dkms libtext-csv-xs-perl libmoosex-types-netaddr-ip-perl iptables-persistent && apt -y autoremove
+	apt -y install curl unzip gzip tar perl xtables-addons-common xtables-addons-dkms libtext-csv-xs-perl libmoosex-types-netaddr-ip-perl libgeoip2-perl iptables-persistent && apt -y autoremove
 	mkdir -p /usr/share/xt_geoip/ && chmod a+rwx /usr/share/xt_geoip/
+	chmod 755 /usr/lib/xtables-addons/xt_geoip_build
 	create_update_sh
 	crontab -l | grep -v "ipban-update.sh" | crontab -
 	(crontab -l 2>/dev/null; echo "0 3 */2 * * /usr/share/ipban/ipban-update.sh") | crontab -
