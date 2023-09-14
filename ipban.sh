3d6579c339c6795a298a8985bfd05712113fee7b
Ali.Debugger <ali.debugger@gmail.com>
2023-09-14 09:25:50 -0700

    Update ipban.sh

--- ipban.sh
+++ ipban.sh
@@ -156,10 +156,10 @@ iptables_rules(){
 	fi
 	
 	if [[ ${IO} == *"O"* ]]; then
-		iptables  -A OUTPUT -m geoip -p tcp -m multiport --dports 0:39999 --dst-cc "${GEOIP}" -j "${LIMIT}"
-		ip6tables -A OUTPUT -m geoip -p tcp -m multiport --dports 0:39999 --dst-cc "${GEOIP}" -j "${LIMIT}"
-		iptables  -A OUTPUT -m geoip -p udp -m multiport --dports 0:39999 --dst-cc "${GEOIP}" -j "${LIMIT}"
-		ip6tables -A OUTPUT -m geoip -p udp -m multiport --dports 0:39999 --dst-cc "${GEOIP}" -j "${LIMIT}"		
+		iptables  -A OUTPUT -m geoip -p tcp -m multiport --dports 0:9999 --dst-cc "${GEOIP}" -j "${LIMIT}"
+		ip6tables -A OUTPUT -m geoip -p tcp -m multiport --dports 0:9999 --dst-cc "${GEOIP}" -j "${LIMIT}"
+		iptables  -A OUTPUT -m geoip -p udp -m multiport --dports 0:9999 --dst-cc "${GEOIP}" -j "${LIMIT}"
+		ip6tables -A OUTPUT -m geoip -p udp -m multiport --dports 0:9999 --dst-cc "${GEOIP}" -j "${LIMIT}"		
 	fi
 		
 	if [[ ${IO} == *"F"* ]]; then
